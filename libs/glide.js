3318f975d18f473220d50fea89f5f17f351a8067
Steve S. Samson <stevee.samson@gmail.com>
2020-01-17 08:14:42 +0100

    remove: * route from lib/route

--- libs/glide.js
+++ libs/glide.js
@@ -1,58 +1,50 @@
 /**
  * Created by steve Samson <stevee.samson@gmail.com> on 2/6/14.
  */
-const express = require('express'),
-    errorHandler = require('errorhandler'),
-    path = require('path'),
-    helmet = require('helmet'),
-    compression = require('compression'),
-    cookieParser = require('cookie-parser'),
-    iocookieParser = require('socket.io-cookie');
-    methodOverride = require('method-override');
+const express = require("express"),
+  errorHandler = require("errorhandler"),
+  path = require("path"),
+  helmet = require("helmet"),
+  compression = require("compression"),
+  cookieParser = require("cookie-parser"),
+  iocookieParser = require("socket.io-cookie");
+methodOverride = require("method-override");
 //stud = require('stud');
 
-module.exports = function (resource, sapper) {
-    const app = express(),
+module.exports = function(resource, sapper) {
+  const app = express(),
     router = express.Router();
 
+  // app.set('port', resource.config.application.port);
 
-    // app.set('port', resource.config.application.port);
+  app.use(
+    helmet(),
+    cookieParser(),
+    resource.slicksMultiparts(),
+    methodOverride(),
+    resource.slickRouter,
+    errorHandler(),
+    compression({ threshold: 0 }),
+    express.static(sapper ? path.basename(PUBLIC_DIR) : PUBLIC_DIR)
+  );
 
-    app.use(
-        helmet(),
-        cookieParser(),
-        resource.slicksMultiparts(),
-        methodOverride(),
-        resource.slickRouter,
-        errorHandler(),
-        compression({ threshold: 0 }),
-        express.static(sapper? path.basename(PUBLIC_DIR) : PUBLIC_DIR)
-    ); 
+  var server = require("http").Server(app),
+    io = require("socket.io")(server);
+  io.use(iocookieParser);
 
+  app.server = server;
+  app.io = io;
+  global.IO = io;
 
+  // require('./strap')(app, resource);
 
-    var server = require('http').Server(app),
-        io = require('socket.io')(server);
-        io.use(iocookieParser);
+  require("./route")(app, resource, router);
 
-    app.server = server;
-    app.io = io;
-    global.IO = io;
+  app.use(MOUNT_PATH, router);
 
+  sapper && app.use(sapper.middleware());
 
-    // require('./strap')(app, resource);
+  app.server.listen(0, "localhost");
 
-    require('./route')(app, resource, router);
-    
-    app.use(MOUNT_PATH,router);
-
-    sapper && app.use(
-        sapper.middleware()
-    );
-    
-
-    app.server.listen(0, 'localhost');
-
-    return app;
-
+  return app;
 };
--- libs/route.js
+++ libs/route.js
@@ -1,215 +1,225 @@
-  /**
-   * Created by steve Samson <stevee.samson@gmail.com> on 2/7/14.
-   */
-  const fs = require('fs'),
-      copyArray = function (c) {
+/**
+ * Created by steve Samson <stevee.samson@gmail.com> on 2/7/14.
+ */
+const fs = require("fs"),
+  copyArray = function(c) {
+    return c.slice(0, c.length);
+  },
+  fspath = require("path"),
+  ext = function(fileName) {
+    var result = fileName.split(".");
 
-          return c.slice(0, c.length);
-      },
-      fspath = require('path'),
-      ext = function (fileName) {
+    return result.length === 1 ? "" : "." + result.pop();
+  };
+//   express = require('express'),
 
-          var result = fileName.split('.');
-
-          return result.length === 1 ? "" : '.' + result.pop();
-      };
-    //   express = require('express'),
-      
-
-  module.exports = function (app, resource, router) {
-
-      var ioRoutes = {
-          get: {},
-          post: {},
-          'delete': {},
-          put: {}
-      };
-      /*
+module.exports = function(app, resource, router) {
+  var ioRoutes = {
+    get: {},
+    post: {},
+    delete: {},
+    put: {}
+  };
+  /*
        Bind middlewares;
        */
-      if (resource.middlewares.length) {
-        //   console.log('mw:', resource.middlewares)
-        //   router.use([(r,s,n) =>{ console.log("1"); n(); },(r,s,n) =>{ console.log("2"); n(); } ]);
-          router.use(resource.middlewares);
-      }
-      var globalPolicy = resource.policies.global;
+  if (resource.middlewares.length) {
+    //   console.log('mw:', resource.middlewares)
+    //   router.use([(r,s,n) =>{ console.log("1"); n(); },(r,s,n) =>{ console.log("2"); n(); } ]);
+    router.use(resource.middlewares);
+  }
+  var globalPolicy = resource.policies.global;
 
-
-      /*
+  /*
        bind / route first
        */
-      if (utils.has(resource.routes, 'Index')) {
-          var route = resource.routes['Index'],
-              controller = resource.controllers['Index'],
-              myPolicy = resource.policies['Index'],
-              myGlobalPolicy = myPolicy && myPolicy['global'];
+  if (utils.has(resource.routes, "Index")) {
+    var route = resource.routes["Index"],
+      controller = resource.controllers["Index"],
+      myPolicy = resource.policies["Index"],
+      myGlobalPolicy = myPolicy && myPolicy["global"];
 
-          for (var path in route) {
-              var handler_name = route[path],
-                  normalized = resource.normalizePath(path),
-                  policy = myPolicy ? (myPolicy[handler_name] ? myPolicy[handler_name] : (myGlobalPolicy ? myGlobalPolicy : (globalPolicy || []))) : (globalPolicy ? globalPolicy : []);
+    for (var path in route) {
+      var handler_name = route[path],
+        normalized = resource.normalizePath(path),
+        policy = myPolicy
+          ? myPolicy[handler_name]
+            ? myPolicy[handler_name]
+            : myGlobalPolicy
+            ? myGlobalPolicy
+            : globalPolicy || []
+          : globalPolicy
+          ? globalPolicy
+          : [];
 
-              router[normalized.method](normalized.path, policy, controller[handler_name]);
+      router[normalized.method](
+        normalized.path,
+        policy,
+        controller[handler_name]
+      );
+    }
+  }
 
-
-          }
-      }
-
-
-      /*
+  /*
        Bind all others routes
        */
-      for (var rkey in resource.routes) {
-          if (rkey === 'Index') {
-              continue;
-          }
+  for (var rkey in resource.routes) {
+    if (rkey === "Index") {
+      continue;
+    }
 
-          var route = resource.routes[rkey],
-              controller = resource.controllers[rkey],
-              myPolicy = resource.policies[rkey],
-              myGlobalPolicy = myPolicy && myPolicy['global'];
+    var route = resource.routes[rkey],
+      controller = resource.controllers[rkey],
+      myPolicy = resource.policies[rkey],
+      myGlobalPolicy = myPolicy && myPolicy["global"];
 
-          for (var path in route) {
-              var handler_name = route[path],
-                  normalized = resource.normalizePath(path),
-                  policy = myPolicy ? (myPolicy[handler_name] ? myPolicy[handler_name] : (myGlobalPolicy ? myGlobalPolicy : (globalPolicy || []))) : (globalPolicy ? globalPolicy : []);
+    for (var path in route) {
+      var handler_name = route[path],
+        normalized = resource.normalizePath(path),
+        policy = myPolicy
+          ? myPolicy[handler_name]
+            ? myPolicy[handler_name]
+            : myGlobalPolicy
+            ? myGlobalPolicy
+            : globalPolicy || []
+          : globalPolicy
+          ? globalPolicy
+          : [];
 
-              // policy.push(resource.slicksTenancy); //Added/Removed
-              // console.log('Handler: ' + handler_name + ' path: ' + normalized.path + ' method: ' + normalized.method);
-              if (controller[handler_name]) {
-                  router[normalized.method](normalized.path, policy, controller[handler_name]);
-                  //ioRoutes[normalized.method][normalized.path] = {policy:policy,handler:controller[handler_name]};
+      // policy.push(resource.slicksTenancy); //Added/Removed
+      // console.log('Handler: ' + handler_name + ' path: ' + normalized.path + ' method: ' + normalized.method);
+      if (controller[handler_name]) {
+        router[normalized.method](
+          normalized.path,
+          policy,
+          controller[handler_name]
+        );
+        //ioRoutes[normalized.method][normalized.path] = {policy:policy,handler:controller[handler_name]};
 
-                  ioRoutes[normalized.method][normalized.path] = (function (_policy, _handler) {
+        ioRoutes[normalized.method][normalized.path] = (function(
+          _policy,
+          _handler
+        ) {
+          return function(req, res) {
+            var _policies = copyArray(_policy),
+              next = function() {
+                _policies.length && _policies.shift()(req, res, next);
+              };
 
-                      return function (req, res) {
+            _policies.push(_handler);
 
-                          var _policies = copyArray(_policy),
-                              next = function () {
-                                  _policies.length && _policies.shift()(req, res, next);
-                              };
+            //console.log('Polices: ', _policies.toString());
 
-                          _policies.push(_handler);
-
-                          //console.log('Polices: ', _policies.toString());
-
-                          next();
-
-                      }
-
-                  })(policy, controller[handler_name]);
-              } else {
-                  console.error('Handler: ' + handler_name + ' not found on for path: ' + normalized.path + ' on method: ' + normalized.method);
-              }
-          }
-
-      }
-
-      if (resource.config.application.spa_strict) {
-
-          console.log('SPA MODE on');
-          app.get('*', function(req, res) {  
-              res.sendFile(fspath.join(PUBLIC_DIR + '/index.html'));
-          });
+            next();
+          };
+        })(policy, controller[handler_name]);
       } else {
-          console.log('SSR MODE on');
+        console.error(
+          "Handler: " +
+            handler_name +
+            " not found on for path: " +
+            normalized.path +
+            " on method: " +
+            normalized.method
+        );
       }
-      //console.log('ioRoutes: ', ioRoutes);
+    }
+  }
 
+  if (resource.config.application.spa_strict) {
+    console.log("SPA MODE on");
+    //   app.get('*', function(req, res) {
+    //       res.sendFile(fspath.join(PUBLIC_DIR + '/index.html'));
+    //   });
+  } else {
+    console.log("SSR MODE on");
+  }
+  //console.log('ioRoutes: ', ioRoutes);
 
-
-
-      global['SlicksDecoder'] = {
-          writeStreamTo: function (req, options, cb) {
-
-              var dest = options.save_as,
-                  ws = fs.createWriteStream(dest);
-              req.on('data', function (chunk) {
-                  ws.write(chunk);
-              }).on('end', function () {
-                  ws.destroySoon();
-                  ws.on('close', function () {
-                      dest = dest.replace(PUBLIC_DIR, '');
-                      cb && cb({
-                          text: 'Web capture was successful.',
-                          src: dest
-                      });
-                  });
-              });
-          },
-          writeFileTo: function (req, options, cb) {
-              //console.log(req.files);
-              var file = req.files[options.load_name],
-                  dest = options.save_as + ext(file.name);
-              file.renameTo(dest, function (e) {
-                  if (e) {
-
-                      cb && cb({
-                          error: 'Error while uploading -\'' + file.name + '\' ' + e.message
-                      });
-
-                  } else {
-                      dest = dest.replace(PUBLIC_DIR, '');
-                      cb && cb({
-                          text: 'Document uploaded successfully.',
-                          src: dest
-                      });
-                  }
-              });
-
-
-          }
-      };
-
-      // global.IO = app.io;
-      app.io.sockets.on('connection', function (socket) {
-
-          console.log('Connected: ', socket.id);
-
-
-          socket.once('disconnect', function () {
-              console.log('disconnecting...');
-              socket.disconnect();
-          });
-
-          ['get', 'post', 'delete', 'put'].forEach(function (method) {
-              socket.on(method, function (req, cb) {
-
-
-                  var res = {
-                      json: cb,
-                      status: function (stat) {
-                          res.status = stat;
-                          return res;
-                      }
-                  };
-
-
-                  resource.slicksIORouter(req, method);
-
-                  req.io = socket;
-                  req.url = req.path;
-                  req.cookies = socket.handshake.headers.cookie || socket.request.headers.cookie;
-
-
-
-                  console.log("mtd: ",method);
-                  console.log("Path: ",req.path);
-                  console.log("Params: ", req.parameters);
-                  console.log("IO x-token: ", req.parameters['x-csrf-token']);
-
-                  if (!req.path.trim()) return;
-                  ioRoutes[method][req.path](req, res);
-
+  global["SlicksDecoder"] = {
+    writeStreamTo: function(req, options, cb) {
+      var dest = options.save_as,
+        ws = fs.createWriteStream(dest);
+      req
+        .on("data", function(chunk) {
+          ws.write(chunk);
+        })
+        .on("end", function() {
+          ws.destroySoon();
+          ws.on("close", function() {
+            dest = dest.replace(PUBLIC_DIR, "");
+            cb &&
+              cb({
+                text: "Web capture was successful.",
+                src: dest
               });
           });
+        });
+    },
+    writeFileTo: function(req, options, cb) {
+      //console.log(req.files);
+      var file = req.files[options.load_name],
+        dest = options.save_as + ext(file.name);
+      file.renameTo(dest, function(e) {
+        if (e) {
+          cb &&
+            cb({
+              error: "Error while uploading -'" + file.name + "' " + e.message
+            });
+        } else {
+          dest = dest.replace(PUBLIC_DIR, "");
+          cb &&
+            cb({
+              text: "Document uploaded successfully.",
+              src: dest
+            });
+        }
       });
-
-      (process.send && process.send({
-          pid: process.pid,
-          type: 'STARTED'
-      }));
-
-      return router;
-
+    }
   };
+
+  // global.IO = app.io;
+  app.io.sockets.on("connection", function(socket) {
+    console.log("Connected: ", socket.id);
+
+    socket.once("disconnect", function() {
+      console.log("disconnecting...");
+      socket.disconnect();
+    });
+
+    ["get", "post", "delete", "put"].forEach(function(method) {
+      socket.on(method, function(req, cb) {
+        var res = {
+          json: cb,
+          status: function(stat) {
+            res.status = stat;
+            return res;
+          }
+        };
+
+        resource.slicksIORouter(req, method);
+
+        req.io = socket;
+        req.url = req.path;
+        req.cookies =
+          socket.handshake.headers.cookie || socket.request.headers.cookie;
+
+        console.log("mtd: ", method);
+        console.log("Path: ", req.path);
+        console.log("Params: ", req.parameters);
+        console.log("IO x-token: ", req.parameters["x-csrf-token"]);
+
+        if (!req.path.trim()) return;
+        ioRoutes[method][req.path](req, res);
+      });
+    });
+  });
+
+  process.send &&
+    process.send({
+      pid: process.pid,
+      type: "STARTED"
+    });
+
+  return router;
+};
